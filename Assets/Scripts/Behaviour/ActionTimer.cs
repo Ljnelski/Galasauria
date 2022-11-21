6d47a036b2c17c21e95d2ff9c5d2a6587993ec5c
Liam Nelski <ljnelski@gmail.com>
2022-11-21 13:29:43 -0500

    Added Support for pausing updating setting and canceling ActionTimers

--- Assets/Scripts/Behaviour/ActionTimer.cs
+++ Assets/Scripts/Behaviour/ActionTimer.cs
@@ -1,19 +1,24 @@
 ﻿/*  Filename:           ActionTimer.cs
  *  Author:             Liam Nelski (301064116)
- *  Last Update:        November 12th, 2022
+ *  Last Update:        November 21th, 2022
  *  Description:        Timer that has a Callback for a void 
  *  Revision History:   November 12th (Liam Nelski): Inital Script.
  *                      November 13th (Liam Nelski): Added Option Callback for onTick
+ *                      November 21st (Liam Nelski): Added Support for Canceling, Pausing and Updating the Timer
  *                      
  */
 
 using System;
 public class ActionTimer
 {
+    public bool Paused { get => _paused; set => _paused = value; }
+    public bool Unpaused { get => !_paused; set => _paused = !value; }
+
     private Action _onCompleteCallback;
     private Action<ActionTimer> _OnDestroyCallback;
     private Action<float> _onTickCallback;
     private float _timerTime;
+    private bool _paused;
 
     public ActionTimer(Action<ActionTimer> onDestroy)
     {
@@ -25,17 +30,30 @@ public class ActionTimer
         _timerTime = timer;
         _onCompleteCallback += completeCallback;
         _onTickCallback += onTickCallback;
+    }   
+
+    public void SetTimerTime(float newTime)
+    {
+        _timerTime = newTime;
+    }
+
+    public void UpdateTimerTime(float amount)
+    {
+        _timerTime += amount;
     }
 
     public void Tick(float deltaTime)
     {
-        _timerTime -= deltaTime;
-        _onTickCallback?.Invoke(_timerTime);
-
-        if (_timerTime < 0)
+        if(Unpaused)
         {
-            Complete();
-        }
+            _timerTime -= deltaTime;
+            _onTickCallback?.Invoke(_timerTime);
+
+            if (_timerTime < 0)
+            {
+                Complete();
+            } 
+        }        
     }
 
     public void Complete()
@@ -44,7 +62,13 @@ public class ActionTimer
         _onCompleteCallback = null;
         _onTickCallback = null;
         _OnDestroyCallback.Invoke(this);
+    }
 
+    public void CancelTimer()
+    {
+        _onCompleteCallback = null;
+        _onTickCallback = null;
+        _OnDestroyCallback?.Invoke(this);
     }
 }
 
