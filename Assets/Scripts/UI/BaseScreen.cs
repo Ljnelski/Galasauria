089081a72e765b56ca5a583fa443ee71d7d84876
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-11-26 12:48:10 -0500

    Fixed bug in inventory screen

--- Assets/Scripts/UI/BaseScreen.cs
+++ Assets/Scripts/UI/BaseScreen.cs
@@ -1,20 +1,21 @@
 /*  Filename:           BaseScreen.cs
  *  Author:             Yuk Yee Wong (301234795)
- *  Last Update:        November 13, 2022
+ *  Last Update:        November 25, 2022
  *  Description:        Define time scale on enable and on disable
  *  Revision History:   November 13, 2022 (Yuk Yee Wong): Initial script.
+ *                      November 25, 2022 (Yuk Yee Wong): Change OnEnable and OnDisable to virtual functions.
  */
 
 using UnityEngine;
 
 public class BaseScreen : MonoBehaviour
 {
-    protected void OnEnable()
+    protected virtual void OnEnable()
     {
         Time.timeScale = 0;
     }
 
-    protected void OnDisable()
+    protected virtual void OnDisable()
     {
         Time.timeScale = 1;
     }
--- Assets/Scripts/UI/PlayerDataReaders/InventoryScreen.cs
+++ Assets/Scripts/UI/PlayerDataReaders/InventoryScreen.cs
@@ -1,31 +1,29 @@
 /*  Filename:           InventoryScreen.cs
- *  Author:             Yuk Yee Wong (301234795)
- *  Last Update:        October 12, 2022
+ *  Author:             Yuk Yee Wong (301234795), Liam Nelski (301064116)
+ *  Last Update:        November 26, 2022
  *  Description:        Displays inventory on hand using data from inventory manager and item manager
  *  Revision History:   October 12, 2022 (Yuk Yee Wong): Initial script.
- *                      November 13 2022 (Liam Nelski): Removed Dependency on InventoryManager
+ *                      November 13, 2022 (Liam Nelski): Removed Dependency on InventoryManager
+ *                      November 26, 2022 (Yuk Yee Wong): Fixed null reference exception of _targetScript; inherited UIPlayerDataReaderScreen to get time freezed when OnEnable and unfreezed when OnDisable; fixed a bug in clearing the grid.
  */
 
 using System.Collections;
 using UnityEngine;
 
-public class InventoryScreen : UIPlayerDataReader<Inventory>
+public class InventoryScreen : UIPlayerDataReaderScreen<Inventory>
 {
     [SerializeField] private Transform inventoryContainer;
     [SerializeField] private InventoryItemDisplay inventoryItemPrefab;
     [SerializeField] private InventoryItemDetailsDisplay inventoryItemDetailsDisplay;
 
-    private void Start()
+    protected override void OnEnable()
     {
-        GetTargetScript();
-    }
+        base.OnEnable();
 
-    private void OnEnable()
-    {
         // clear the grid
         if (inventoryContainer.childCount > 0)
         {
-            for (int i = inventoryContainer.childCount - 1; i > 0; i--)
+            for (int i = inventoryContainer.childCount - 1; i > -1; i--)
             {
                 // TODO, recycle
                 Destroy(inventoryContainer.GetChild(i).gameObject);
