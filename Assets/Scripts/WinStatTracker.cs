50f2cd385f9c70dad5f546b666591d3cd46f1772
Jinkyu Choi <jchoi123@my.centennialcollege.ca>
2022-12-03 18:57:34 -0500

    Level Unlock Method Implemented

---
+++ Assets/Scripts/WinStatTracker.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinStatTracker : MonoBehaviour
+{
+
+    public static bool isLv1Complete;
+    public static bool isLv2Complete;
+    public static bool isLv3Complete;
+}
