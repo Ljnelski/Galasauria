cd35f4561277f34ff7501d89f1ebe50a57b4812b
Meteor <MeteorZero@outlook.com>
2022-12-12 08:21:17 +0800

    added Automatic door Prefab and implemented

---
+++ Assets/Scripts/AutomaticDoor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticDoor : MonoBehaviour
+{
+    public Animator doorAnim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnim.SetTrigger("open");
+        }
+    }
+}
