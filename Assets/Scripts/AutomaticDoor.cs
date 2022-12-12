fd87c9e48029669ee71e39557f3bed79dd8e7aa8
Meteor <MeteorZero@outlook.com>
2022-12-12 09:01:41 +0800

    implemented the sound effect and fix a bug

--- Assets/Scripts/AutomaticDoor.cs
+++ Assets/Scripts/AutomaticDoor.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class AutomaticDoor : MonoBehaviour
 {
     public Animator doorAnim;
+    [SerializeField] private AudioSource doorOpenAudioSource;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             doorAnim.SetTrigger("open");
+            doorOpenAudioSource.Play();
+            Destroy(gameObject);
         }
     }
 }
